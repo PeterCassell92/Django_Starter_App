c7ba1b2534cd9ee17ded25134bc893b8cd42d271
Pete <pc0122@my.bristol.ac.uk>
2025-01-09 00:22:41 +0000

    added dash app files

---
+++ mysite/dash/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+#from django.http import HttpResponse
+
+
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the dash index.")
+
+
+def index(request):
+    print('Here')
+    return render(request, 'dash/index.html')
+
+def dash_view(request):
+    return render(request, 'dash/index.html')
+
+class DashIndexView(TemplateView):
+    template_name = 'dash/index.html'
+
+    """
+        Go the polls app TODO:
+    """
+    def go_to_polls(self):
+        return None
